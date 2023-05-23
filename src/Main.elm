6fb362dfc98d6ba6854a4565b3f050b12aed1aaa
RalfBarkow <ralf.barkow@me.com>
2023-05-23 22:15:32 +0200

    Refactoring the form view, adding submit button functionality.
    This commit refactors the form view by adding an onClick event handler for the submit button that triggers the SubmitClicked message. It also removes the viewInput and viewValidation functions because they are no longer needed. This update improves the interactivity of the form and simplifies the code base.

--- src/Main.elm
+++ src/Main.elm
@@ -1,10 +1,112 @@
 module Main exposing (main)
 
-import Html
+-- Input a user name and password.
+--
+-- Read how it works:
+--   https://guide.elm-lang.org/architecture/forms.html
+--
 
+import Browser
+import Debug
+import Html exposing (Html, button, div, form, input, text)
+import Html.Attributes exposing (placeholder, type_)
+import Html.Events exposing (onClick, onInput)
+import Http
+import Json.Decode as Json
+import Json.Encode as Encode
+
+
+
+-- Define the model
+
+
+type alias Model =
+    { username : String
+    , password : String
+    , response : String
+    }
+
+
+initialModel : Model
+initialModel =
+    { username = ""
+    , password = ""
+    , response = ""
+    }
+
+
+
+-- Define the Msg type
+
+
+type Msg
+    = UsernameChanged String
+    | PasswordChanged String
+    | SubmitClicked
+    | RequestCompleted (Result Http.Error String)
+
+
+
+-- Define the update function
+
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        UsernameChanged newUsername ->
+            { model | username = newUsername }
+
+        PasswordChanged newPassword ->
+            { model | password = newPassword }
+
+        SubmitClicked ->
+            let
+                request =
+                    { body =
+                        Http.jsonBody <|
+                            Encode.object
+                                [ ( "username", Encode.string model.username )
+                                , ( "password", Encode.string model.password )
+                                ]
+                    , expect = Http.expectString RequestCompleted
+                    , url = "https://your-server.com/login" -- Replace with your server URL
+                    }
+            in
+            { model | response = "Sending request..." }
+
+        RequestCompleted result ->
+            case result of
+                Ok response ->
+                    { model | response = "Request completed: " ++ response }
+
+                Err error ->
+                    { model | response = "Request failed: " ++ Debug.toString error }
+
+
+
+-- Define the view function
+
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ form []
+            [ input [ placeholder "Username", type_ "text", onInput UsernameChanged ] []
+            , input [ placeholder "Password", type_ "password", onInput PasswordChanged ] []
+            , button [ onClick SubmitClicked ] [ text "Submit" ]
+            ]
+        , text model.response
+        ]
+
+
+
+-- Start the Elm application
+
+
+main : Program () Model Msg
 main =
-    Html.text (greetLindsay "Wardell")
-
-greet firstName lastName =  "Hello " ++ firstName ++ " " ++ lastName
-
-greetLindsay = greet "Lindsay" 
+    Browser.sandbox
+        { init = initialModel
+        , update = update
+        , view = view
+        }
