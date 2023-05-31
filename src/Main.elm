09770bfa48c6e63eff2826ad6ddeec71634384b8
RalfBarkow <ralf.barkow@me.com>
2023-05-31 20:07:28 +0200

    refactor(Main): JSON decoding and add error handling
    This commit refactors the JSON decoding code to handle a list of stories and a list of journal entries. It also adds error handling to provide appropriate feedback to the user in case of decoding failures.

--- src/Main.elm
+++ src/Main.elm
@@ -1,24 +1,26 @@
 module Main exposing (main)
 
 import Browser
+import Debug
 import Html exposing (..)
 import Html.Attributes exposing (style)
 import Html.Events exposing (..)
 import Http
-import Json.Decode exposing (Decoder, field, string, map3)
-import Debug
+import Json.Decode exposing (Decoder, field, list, map3, map4, string)
+
 
 
 -- MAIN
 
 
+main : Program () Model Msg
 main =
-  Browser.element
-    { init = init
-    , update = update
-    , subscriptions = subscriptions
-    , view = view
-    }
+    Browser.element
+        { init = init
+        , update = update
+        , subscriptions = subscriptions
+        , view = view
+        }
 
 
 
@@ -26,21 +28,36 @@ main =
 
 
 type Model
-  = Failure
-  | Loading
-  | Success Page
+    = Failure
+    | Loading
+    | Success Page
 
 
 type alias Page =
-  { title : String
-  , story : String
-  , journal : String
-  }
+    { title : String
+    , story : List Story
+    , journal : List Journal
+    }
 
 
-init : () -> (Model, Cmd Msg)
+type alias Story =
+    { typeOfStory : String
+    , id : String
+    , text : String
+    }
+
+
+type alias Journal =
+    { typeOfJournal : String
+    , id : String
+    , item : String
+    , date : String
+    }
+
+
+init : () -> ( Model, Cmd Msg )
 init _ =
-  (Loading, getWikiPageJson)
+    ( Loading, getWikiPageJson )
 
 
 
@@ -48,44 +65,49 @@ init _ =
 
 
 type Msg
-  = DoIt
-  | GotPage (Result Http.Error Page)
+    = DoIt
+    | GotPage (Result Http.Error Page)
 
 
-update : Msg -> Model -> (Model, Cmd Msg)
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-  case msg of
-    DoIt ->
-      (Loading, getWikiPageJson)
+    case msg of
+        DoIt ->
+            ( Loading, getWikiPageJson )
 
-    GotPage result ->
-      case result of
-        Ok page ->
-          (Success page, Cmd.none)
+        GotPage result ->
+            case result of
+                Ok page ->
+                    ( Success page, Cmd.none )
 
-        Err err ->
-          let
-            errorMsg =
-              case err of
-                Http.Timeout ->
-                  "Timeout"
+                Err err ->
+                    let
+                        errorMsg =
+                            case err of
+                                Http.Timeout ->
+                                    "Timeout"
 
-                Http.NetworkError ->
-                  "Network Error"
+                                Http.NetworkError ->
+                                    "Network Error"
 
-                Http.BadStatus status ->
-                  "Bad Status: " ++ String.fromInt status
+                                Http.BadStatus status ->
+                                    "Bad Status: " ++ String.fromInt status
 
-                Http.BadBody body ->
-                  let
-                    _ = Debug.log "GotPage JSON:" body -- Log the JSON content in case of Failure
-                  in
-                  "Bad Body: " ++ body
+                                Http.BadBody body ->
+                                    let
+                                        _ =
+                                            Debug.log "GotPage JSON:" body
 
-                Http.BadUrl _ ->
-                  Debug.todo "Handle BadUrl case" -- Placeholder for handling BadUrl case
-          in
-          (Failure, Cmd.none)
+                                        -- Log the JSON content in case of Failure
+                                    in
+                                    "Bad Body: " ++ body
+
+                                Http.BadUrl _ ->
+                                    Debug.todo "Handle BadUrl case"
+
+                        -- Placeholder for handling BadUrl case
+                    in
+                    ( Failure, Cmd.none )
 
 
 
@@ -94,7 +116,7 @@ update msg model =
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
-  Sub.none
+    Sub.none
 
 
 
@@ -103,29 +125,29 @@ subscriptions model =
 
 view : Model -> Html Msg
 view model =
-  div []
-    [ h2 [] [ text "Wiki Page JSON" ]
-    , viewPage model
-    ]
+    div []
+        [ h2 [] [ text "Wiki Page JSON" ]
+        , viewPage model
+        ]
 
 
 viewPage : Model -> Html Msg
 viewPage model =
-  case model of
-    Failure ->
-      div []
-        [ text "I could not load the Wiki Page JSON for some reason. "
-        , button [ onClick DoIt ] [ text "Try Again!" ]
-        ]
+    case model of
+        Failure ->
+            div []
+                [ text "I could not load the Wiki Page JSON for some reason. "
+                , button [ onClick DoIt ] [ text "Try Again!" ]
+                ]
 
-    Loading ->
-      text "Loading..."
+        Loading ->
+            text "Loading..."
 
-    Success page ->
-      div []
-        [ button [ onClick DoIt, style "display" "block" ] [ text "Do it" ]
-        , blockquote [] [ text page.title ]
-        ]
+        Success page ->
+            div []
+                [ button [ onClick DoIt, style "display" "block" ] [ text "Do it" ]
+                , blockquote [] [ text page.title ]
+                ]
 
 
 
@@ -134,25 +156,32 @@ viewPage model =
 
 getWikiPageJson : Cmd Msg
 getWikiPageJson =
-  Http.get
-    { url = "https://wiki.ralfbarkow.ch/2023-05-31.json"
-    , expect = Http.expectJson GotPage pageDecoder
-    }
+    Http.get
+        { url = "http://wiki.ralfbarkow.ch/elm-and-json.json"
+        , expect = Http.expectJson GotPage decodePage
+        }
 
 
-pageDecoder : Decoder Page
-pageDecoder =
-  map3 Page
-    (field "title" string)
-    (field "story" storyDecoder)
-    (field "journal" journalDecoder)
+decodePage : Decoder Page
+decodePage =
+    map3 Page
+        (field "title" string)
+        (field "story" (list decodeStory))
+        (field "journal" (list decodeJournal))
 
 
-storyDecoder : Decoder String
-storyDecoder =
-  field "story" string |> Debug.log "Decoding story:"
+decodeStory : Decoder Story
+decodeStory =
+    map3 Story
+        (field "type" string)
+        (field "id" string)
+        (field "text" string)
 
 
-journalDecoder : Decoder String
-journalDecoder =
-  field "journal" string |> Debug.log "Decoding journal:"
+decodeJournal : Decoder Journal
+decodeJournal =
+    map4 Journal
+        (field "type" string)
+        (field "id" string)
+        (field "item" string)
+        (field "date" string)
