dc4a342de0216eb48eae8bef0d8b3efea190cee1
RalfBarkow <ralf.barkow@me.com>
2023-06-01 19:47:05 +0200

    test: add tests/DecoderTests.elm
    elm-test 0.19.1-revision12

--- src/Main.elm
+++ src/Main.elm
@@ -1,57 +1,73 @@
 module Main exposing (main)
 
 import Html exposing (Html)
-import Json.Decode as Decode exposing (Decoder, field, int, list, map2, map3, string)
+import Json.Decode as Decode exposing (Decoder, map2, map3, string)
+import Json.Decode.Pipeline exposing (required)
 
 
-type alias Item =
+type alias Page =
     { title : String
-    , story : List String
+    , story : List Story
+    , journal : List Journal
+    }
+
+
+type alias Story =
+    { storyType : Maybe String
+    , id : Maybe String
+    , text : Maybe String
     }
 
 
 type alias Journal =
     { journalType : String
-    , item : Item
+    , item : Maybe Item
     , date : Int
     }
 
 
-type alias Page =
+type alias Item =
     { title : String
-    , story : List String
-    , journal : List Journal
+    , story : Maybe Story
     }
-
-
-main : Html msg
-main =
-    """
-{"title":"Create New Page Test","story":[],"journal":[{"type":"create","item":{"title":"Create New Page Test","story":[]},"date":1685633118352}]}
-"""
-        |> Decode.decodeString decodePage
-        |> Debug.toString
-        |> Html.text
 
 
 decodePage : Decoder Page
 decodePage =
-    map3 Page
+    Decode.map3 Page
         (Decode.field "title" Decode.string)
-        (Decode.field "story" (Decode.list Decode.string))
+        (Decode.field "story" (Decode.list decodeStory))
         (Decode.field "journal" (Decode.list decodeJournal))
+
+
+decodeStory : Decoder Story
+decodeStory =
+    Decode.map3 Story
+        (Decode.maybe (Decode.field "type" Decode.string))
+        (Decode.maybe (Decode.field "id" Decode.string))
+        (Decode.maybe (Decode.field "text" Decode.string))
 
 
 decodeJournal : Decoder Journal
 decodeJournal =
-    map3 Journal
+    Decode.map3 Journal
         (Decode.field "type" Decode.string)
-        (Decode.field "item" decodeItem)
+        (Decode.maybe decodeItem)
         (Decode.field "date" Decode.int)
 
 
 decodeItem : Decoder Item
 decodeItem =
-    map2 Item
+    Decode.map2 Item
         (Decode.field "title" Decode.string)
-        (Decode.field "story" (Decode.list Decode.string))
+        (Decode.maybe (Decode.field "story" decodeStory))
+
+
+main : Html msg
+main =
+    """
+{"title":"Create New Page Test","story":[],"journal":[{"type":"create","item":{"title":"Create New Page Test","story":[]},"date":1685640550036}]}
+"""
+        |> Decode.decodeString decodePage
+        |> Debug.toString
+        |> Html.text
---
+++ tests/DecoderTests.elm
@@ -0,0 +1,10 @@
+module DecoderTests exposing (suite)
+
+import Test exposing (Test, describe, test)
+
+
+suite : Test
+suite =
+    describe "Decoder Tests"
+        [-- Add your test cases here
+        ]
