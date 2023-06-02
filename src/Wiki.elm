38648e7b6de1a09225a6b371e045960ec62c2913
RalfBarkow <ralf.barkow@me.com>
2023-06-02 16:05:41 +0200

    refactor(Wiki): use the json-decode-pipeline library

--- src/Wiki.elm
+++ src/Wiki.elm
@@ -1,6 +1,7 @@
 module Wiki exposing (Page, decodePage)
 
-import Json.Decode exposing (Decoder, field, int, list, map3, map4, string)
+import Json.Decode as Decode exposing (Decoder, field, int, list, map3, map4, string)
+import Json.Decode.Pipeline exposing (hardcoded, optional, required)
 
 
 type alias Page =
@@ -10,11 +11,35 @@ type alias Page =
     }
 
 
+decodePage : Decoder Page
+decodePage =
+    Decode.map3 Page
+        (field "title" string)
+        (field "story" (list decodeStory))
+        (field "journal" (list decodeStoryEdit))
+
+
+
+-- The "story" is a collection of paragraphs and paragraph like items.
+
+
 type alias Story =
     { storyType : String
     , id : String
     , text : String
     }
+
+
+decodeStory : Decoder Story
+decodeStory =
+    Decode.map3 Story
+        (field "type" string)
+        (field "id" string)
+        (field "text" string)
+
+
+
+-- The "journal" collects story edits.
 
 
 type alias Journal =
@@ -32,34 +57,18 @@ type alias Item =
     }
 
 
-decodePage : Decoder Page
-decodePage =
-    map3 Page
-        (field "title" string)
-        (field "story" (list decodeStory))
-        (field "journal" (list decodeStoryEdit))
-
-
-decodeStory : Decoder Story
-decodeStory =
-    map3 Story
-        (field "type" string)
-        (field "id" string)
-        (field "text" string)
-
-
 decodeStoryEdit : Decoder Journal
 decodeStoryEdit =
-    map4 Journal
-        (field "type" string)
-        (field "id" string)
-        (field "item" decodeJournalItem)
-        (field "date" int)
+    Decode.succeed Journal
+        |> required "type" string
+        |> required "id" string
+        |> required "item" decodeJournalItem
+        |> required "date" int
 
 
 decodeJournalItem : Decoder Item
 decodeJournalItem =
-    map3 Item
+    Decode.map3 Item
         (field "type" string)
         (field "id" string)
         (field "text" string)
