5cf77803408d5e68c447c8e4ac30004a912ca163
RalfBarkow <ralf.barkow@me.com>
2023-06-02 19:14:54 +0200

    test(WikiSpec): RED Decode and Encode Page

--- src/Wiki.elm
+++ src/Wiki.elm
@@ -1,7 +1,8 @@
-module Wiki exposing (Page, decodePage)
+module Wiki exposing (Page, decodePage, encodePage)
 
-import Json.Decode as Decode exposing (Decoder, field, int, list, map3, map4, string)
+import Json.Decode as Decode exposing (Decoder, field, int, list, map3, string)
 import Json.Decode.Pipeline exposing (hardcoded, optional, required)
+import Json.Encode as Encode
 
 
 type alias Page =
@@ -16,7 +17,7 @@ decodePage =
     Decode.map3 Page
         (field "title" string)
         (field "story" (list decodeStory))
-        (field "journal" (list decodeStoryEdit))
+        (field "journal" (list decodeJournal))
 
 
 
@@ -43,11 +44,42 @@ decodeStory =
 
 
 type alias Journal =
-    { journalType : String
+    { journalType : StoryEditType
     , id : String
     , item : Item
     , date : Int
     }
+
+
+type StoryEditType
+    = Edit
+    | Add
+    | Move
+
+
+decodeJournal : Decoder Journal
+decodeJournal =
+    Decode.succeed Journal
+        |> required "type" (Decode.andThen decodeStoryEditType string)
+        |> required "id" string
+        |> required "item" decodeJournalItem
+        |> required "date" int
+
+
+decodeStoryEditType : String -> Decoder StoryEditType
+decodeStoryEditType name =
+    case name of
+        "edit" ->
+            Decode.succeed Edit
+
+        "add" ->
+            Decode.succeed Add
+
+        "move" ->
+            Decode.succeed Move
+
+        _ ->
+            Decode.fail ("Unknown record type: " ++ name)
 
 
 type alias Item =
@@ -57,18 +89,59 @@ type alias Item =
     }
 
 
-decodeStoryEdit : Decoder Journal
-decodeStoryEdit =
-    Decode.succeed Journal
-        |> required "type" string
-        |> required "id" string
-        |> required "item" decodeJournalItem
-        |> required "date" int
-
-
 decodeJournalItem : Decoder Item
 decodeJournalItem =
     Decode.map3 Item
         (field "type" string)
         (field "id" string)
         (field "text" string)
+
+
+encodePage : Page -> Encode.Value
+encodePage page =
+    Encode.object
+        [ ( "journal", Encode.list encodeJournal page.journal )
+        , ( "story", Encode.list encodeStory page.story )
+        , ( "title", Encode.string page.title )
+        ]
+
+
+encodeJournal : Journal -> Encode.Value
+encodeJournal journal =
+    Encode.object
+        [ ( "journalType", Encode.string (storyEditTypeToString journal.journalType) )
+        , ( "id", Encode.string journal.id )
+        , ( "item", encodeJournalItem journal.item )
+        , ( "date", Encode.int journal.date )
+        ]
+
+
+storyEditTypeToString : StoryEditType -> String
+storyEditTypeToString storyEditType =
+    case storyEditType of
+        Add ->
+            "add"
+
+        Edit ->
+            "edit"
+
+        Move ->
+            "move"
+
+
+encodeJournalItem : Item -> Encode.Value
+encodeJournalItem item =
+    Encode.object
+        [ ( "itemType", Encode.string item.itemType )
+        , ( "id", Encode.string item.id )
+        , ( "text", Encode.string item.text )
+        ]
+
+
+encodeStory : Story -> Encode.Value
+encodeStory story =
+    Encode.object
+        [ ( "storyType", Encode.string story.storyType )
+        , ( "id", Encode.string story.id )
+        , ( "text", Encode.string story.text )
+        ]
--- tests/WikiSpec.elm
+++ tests/WikiSpec.elm
@@ -2,49 +2,49 @@ module WikiSpec exposing (suite)
 
 import Expect
 import Json.Decode as Decode
+import Json.Encode as Encode
 import Test exposing (Test, describe, test)
-import Wiki exposing (decodePage)
+import Wiki exposing (decodePage, encodePage)
 
 
 rawData : String
 rawData =
     """
+{
+  "title": "Create New Page Test",
+  "story": [],
+  "journal": [
     {
-        "journal": [
-            {
-                "journalType": "create",
-                "item": {
-                    "itemId": "item1"
-                },
-                "date": 1685640550036
-            }
-        ],
-        "story": [],
-        "title": "Create New Page Test"
+      "type": "create",
+      "item": {
+        "title": "Create New Page Test",
+        "story": []
+      },
+      "date": 1685700575889
     }
+  ]
+}    
     """
-
-
-expected : String
-expected =
-    """Ok { journal = [{ date = 1685640550036, item = { story = Nothing, title = "Create New Page Test" }, journalType = "create" }], story = [], title = "Create New Page Test" }"""
 
 
 suite : Test
 suite =
-    describe "DecoderTests"
-        [ test "decodePage with decodeAndDebug"
-            (\_ ->
-                Expect.equal expected (decodeAndDebug rawData)
+    describe "Decoder and Encoder Tests"
+        [ test "Decode and Encode Page"
+            (\() ->
+                let
+                    input : String
+                    input =
+                        -- Provide the input data for decoding
+                        rawData
+
+                    decodedResult =
+                        Decode.decodeString decodePage input
+
+                    encodedResult =
+                        Result.map (Encode.encode 2 << encodePage) decodedResult
+                in
+                encodedResult
+                    |> Expect.equal (Ok input)
             )
         ]
-
-
-decodeAndDebug : String -> String
-decodeAndDebug data =
-    case Decode.decodeString decodePage data of
-        Ok decoded ->
-            Debug.toString decoded
-
-        Err error ->
-            Debug.toString error
