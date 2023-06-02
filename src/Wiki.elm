d187441c1f7765fdc436f59eaad77ca86863bfe1
RalfBarkow <ralf.barkow@me.com>
2023-06-02 19:35:21 +0200

    refactor(Wiki): rearrange decoders and encoders
    related stand among each other

--- src/Wiki.elm
+++ src/Wiki.elm
@@ -20,6 +20,15 @@ decodePage =
         (field "journal" (list decodeJournal))
 
 
+encodePage : Page -> Encode.Value
+encodePage page =
+    Encode.object
+        [ ( "title", Encode.string page.title )
+        , ( "story", Encode.list encodeStory page.story )
+        , ( "journal", Encode.list encodeJournal page.journal )
+        ]
+
+
 
 -- The "story" is a collection of paragraphs and paragraph like items.
 
@@ -37,6 +46,15 @@ decodeStory =
         (field "type" string)
         (field "id" string)
         (field "text" string)
+
+
+encodeStory : Story -> Encode.Value
+encodeStory story =
+    Encode.object
+        [ ( "storyType", Encode.string story.storyType )
+        , ( "id", Encode.string story.id )
+        , ( "text", Encode.string story.text )
+        ]
 
 
 
@@ -64,6 +82,29 @@ decodeJournal =
         |> required "id" string
         |> required "item" decodeJournalItem
         |> required "date" int
+
+
+encodeJournal : Journal -> Encode.Value
+encodeJournal journal =
+    Encode.object
+        [ ( "journalType", Encode.string (storyEditTypeToString journal.journalType) )
+        , ( "id", Encode.string journal.id )
+        , ( "item", encodeJournalItem journal.item )
+        , ( "date", Encode.int journal.date )
+        ]
+
+
+storyEditTypeToString : StoryEditType -> String
+storyEditTypeToString storyEditType =
+    case storyEditType of
+        Add ->
+            "add"
+
+        Edit ->
+            "edit"
+
+        Move ->
+            "move"
 
 
 decodeStoryEditType : String -> Decoder StoryEditType
@@ -97,51 +138,10 @@ decodeJournalItem =
         (field "text" string)
 
 
-encodePage : Page -> Encode.Value
-encodePage page =
-    Encode.object
-        [ ( "journal", Encode.list encodeJournal page.journal )
-        , ( "story", Encode.list encodeStory page.story )
-        , ( "title", Encode.string page.title )
-        ]
-
-
-encodeJournal : Journal -> Encode.Value
-encodeJournal journal =
-    Encode.object
-        [ ( "journalType", Encode.string (storyEditTypeToString journal.journalType) )
-        , ( "id", Encode.string journal.id )
-        , ( "item", encodeJournalItem journal.item )
-        , ( "date", Encode.int journal.date )
-        ]
-
-
-storyEditTypeToString : StoryEditType -> String
-storyEditTypeToString storyEditType =
-    case storyEditType of
-        Add ->
-            "add"
-
-        Edit ->
-            "edit"
-
-        Move ->
-            "move"
-
-
 encodeJournalItem : Item -> Encode.Value
 encodeJournalItem item =
     Encode.object
         [ ( "itemType", Encode.string item.itemType )
         , ( "id", Encode.string item.id )
         , ( "text", Encode.string item.text )
-        ]
-
-
-encodeStory : Story -> Encode.Value
-encodeStory story =
-    Encode.object
-        [ ( "storyType", Encode.string story.storyType )
-        , ( "id", Encode.string story.id )
-        , ( "text", Encode.string story.text )
         ]
