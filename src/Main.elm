907dee4fcae8b2e82fe83cce01ff3381dc13e67f
RalfBarkow <ralf.barkow@me.com>
2023-06-03 14:27:42 +0200

    Add initial Elm application with two iframes.

--- src/Main.elm
+++ src/Main.elm
@@ -1,150 +1,42 @@
-module Main exposing (main)
+module Main exposing (..)
 
 import Browser
-import Debug
-import Html exposing (..)
-import Html.Attributes exposing (style)
-import Html.Events exposing (..)
-import Http
-import Wiki
-
-
-
--- MAIN
+import Html exposing (Html, div, iframe, text)
+import Html.Attributes exposing (src)
 
 
 main : Program () Model Msg
 main =
-    Browser.element
-        { init = init
-        , update = update
-        , subscriptions = subscriptions
-        , view = view
-        }
+    Browser.sandbox { init = init, update = update, view = view }
 
 
-
--- MODEL
-
-
-type Model
-    = Loading
-    | Success Wiki.Page
-    | Failure String
-
-
-init : () -> ( Model, Cmd Msg )
-init _ =
-    ( Loading, getWikiPageJson )
-
-
-
--- UPDATE
+type alias Model =
+    { leftIframeSrc : String
+    , rightIframeSrc : String
+    }
 
 
 type Msg
-    = DoIt
-    | GotPage (Result Http.Error Wiki.Page)
+    = NoOp
 
 
-update : Msg -> Model -> ( Model, Cmd Msg )
+init : Model
+init =
+    { leftIframeSrc = "https://wiki.ralfbarkow.ch/view/create-new-page-test"
+    , rightIframeSrc = "https://wiki.ralfbarkow.ch/create-new-page-test.json"
+    }
+
+
+update : Msg -> Model -> Model
 update msg model =
     case msg of
-        DoIt ->
-            ( Loading, getWikiPageJson )
-
-        GotPage result ->
-            case result of
-                Ok page ->
-                    ( Success page, Cmd.none )
-
-                Err err ->
-                    let
-                        errorMsg =
-                            case err of
-                                Http.BadUrl _ ->
-                                    -- Placeholder for handling BadUrl case
-                                    Debug.todo "Handle BadUrl case"
-
-                                Http.Timeout ->
-                                    "Timeout"
-
-                                Http.NetworkError ->
-                                    "Network Error"
-
-                                Http.BadStatus status ->
-                                    "Bad Status: " ++ String.fromInt status
-
-                                Http.BadBody body ->
-                                    let
-                                        _ =
-                                            -- Log the JSON content in case of Failure
-                                            Debug.log "GotPage JSON:" body
-                                    in
-                                    "GotPage JSON: " ++ body
-                    in
-                    ( Failure errorMsg, Cmd.none )
-
-
-
--- SUBSCRIPTIONS
-
-
-subscriptions : Model -> Sub Msg
-subscriptions model =
-    Sub.none
-
-
-
--- VIEW
+        NoOp ->
+            model
 
 
 view : Model -> Html Msg
 view model =
     div []
-        [ h2 [] [ text "Wiki Page JSON" ]
-        , viewPage model
-        , viewError model
+        [ iframe [ src model.leftIframeSrc ] []
+        , iframe [ src model.rightIframeSrc ] []
         ]
-
-
-viewPage : Model -> Html Msg
-viewPage model =
-    case model of
-        Loading ->
-            text "Loading..."
-
-        Success page ->
-            div []
-                [ button [ onClick DoIt, style "display" "block" ] [ text "Do it" ]
-                , div [ style "font-weight" "bold" ] [ text page.title ]
-                ]
-
-        Failure _ ->
-            text ""
-
-
-viewError : Model -> Html Msg
-viewError model =
-    case model of
-        Failure errorMsg ->
-            div []
-                [ div [] [ text errorMsg ]
-                , text ""
-                , button [ onClick DoIt ] [ text "Try Again!" ]
-                ]
-
-        _ ->
-            text ""
-
-
-
--- HTTP
-
-
-getWikiPageJson : Cmd Msg
-getWikiPageJson =
-    Http.get
-        { url = "http://wiki.ralfbarkow.ch/elm-and-json.json"
-        , expect = Http.expectJson GotPage Wiki.decodePage
-        }
