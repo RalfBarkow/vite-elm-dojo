8ba5383e064758286e032e93572d9683ca2e4637
RalfBarkow <ralf.barkow@me.com>
2023-06-03 16:09:06 +0200

    refactor: use Browser.element instead of sandbox and 3 iFrames

--- src/Main.elm
+++ src/Main.elm
@@ -1,42 +1,69 @@
-module Main exposing (..)
+module Main exposing (main)
 
 import Browser
-import Html exposing (Html, div, iframe, text)
+import Html exposing (Html, button, div, iframe, text)
 import Html.Attributes exposing (src)
+import Html.Events exposing (onClick)
 
 
 main : Program () Model Msg
 main =
-    Browser.sandbox { init = init, update = update, view = view }
+    Browser.element { init = init, update = update, subscriptions = subscriptions, view = view }
 
 
 type alias Model =
-    { leftIframeSrc : String
-    , rightIframeSrc : String
+    { iframe1Src : String
+    , iframe2Src : String
+    , iframe3Src : String
     }
 
 
 type Msg
-    = NoOp
+    = ChangeIframe1Src String
+    | ChangeIframe2Src String
+    | ChangeIframe3Src String
 
 
-init : Model
-init =
-    { leftIframeSrc = "https://wiki.ralfbarkow.ch/view/create-new-page-test"
-    , rightIframeSrc = "https://wiki.ralfbarkow.ch/create-new-page-test.json"
+init : () -> ( Model, Cmd Msg )
+init _ =
+    ( initModel, Cmd.none )
+
+
+initModel : Model
+initModel =
+    { iframe1Src = "http://wiki.ralfbarkow.ch/view/create-new-page-test"
+    , iframe2Src = "http://wiki.ralfbarkow.ch/create-new-page-test.json"
+    , iframe3Src = "http://wiki.ralfbarkow.ch/"
     }
 
 
-update : Msg -> Model -> Model
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        NoOp ->
-            model
+        ChangeIframe1Src newSrc ->
+            ( { model | iframe1Src = newSrc }, Cmd.none )
+
+        ChangeIframe2Src newSrc ->
+            ( { model | iframe2Src = newSrc }, Cmd.none )
+
+        ChangeIframe3Src newSrc ->
+            ( { model | iframe3Src = newSrc }, Cmd.none )
+
+
+subscriptions : Model -> Sub Msg
+subscriptions _ =
+    Sub.none
 
 
 view : Model -> Html Msg
 view model =
     div []
-        [ iframe [ src model.leftIframeSrc ] []
-        , iframe [ src model.rightIframeSrc ] []
+        [ iframe [ src model.iframe1Src ] []
+        , iframe [ src model.iframe2Src ] []
+        , iframe [ src model.iframe3Src ] []
+        , div []
+            [ button [ onClick (ChangeIframe1Src "http://hive.dreyeck.ch/view/create-new-page-test") ] [ text "Change iframe 1 src" ]
+            , button [ onClick (ChangeIframe2Src "http://hive.dreyeck.ch/create-new-page-test.json") ] [ text "Change iframe 2 src" ]
+            , button [ onClick (ChangeIframe3Src "http://hive.dreyeck.ch/") ] [ text "Change iframe 3 src" ]
+            ]
         ]
