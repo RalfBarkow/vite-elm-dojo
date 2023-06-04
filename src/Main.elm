b44f237c3eb00b1cb92baf94c9e1b437ca224282
RalfBarkow <ralf.barkow@me.com>
2023-06-04 12:44:07 +0200

    Invalid JSON

--- src/Main.elm
+++ src/Main.elm
@@ -1,73 +1,57 @@
 module Main exposing (main)
 
-import Html exposing (Html)
-import Json.Decode as Decode exposing (Decoder, map2, map3, string)
-import Json.Decode.Pipeline exposing (required)
+import Browser
+import Html exposing (Html, div, pre, text)
+import Json.Decode as Json
 
 
-type alias Page =
-    { title : String
-    , story : List Story
-    , journal : List Journal
-    }
-
-
-type alias Story =
-    { storyType : Maybe String
-    , id : Maybe String
-    , text : Maybe String
-    }
-
-
-type alias Journal =
-    { journalType : String
-    , item : Maybe Item
-    , date : Int
-    }
-
-
-type alias Item =
-    { title : String
-    , story : Maybe Story
-    }
-
-
-decodePage : Decoder Page
-decodePage =
-    Decode.map3 Page
-        (Decode.field "title" Decode.string)
-        (Decode.field "story" (Decode.list decodeStory))
-        (Decode.field "journal" (Decode.list decodeJournal))
-
-
-decodeStory : Decoder Story
-decodeStory =
-    Decode.map3 Story
-        (Decode.maybe (Decode.field "type" Decode.string))
-        (Decode.maybe (Decode.field "id" Decode.string))
-        (Decode.maybe (Decode.field "text" Decode.string))
-
-
-decodeJournal : Decoder Journal
-decodeJournal =
-    Decode.map3 Journal
-        (Decode.field "type" Decode.string)
-        (Decode.maybe decodeItem)
-        (Decode.field "date" Decode.int)
-
-
-decodeItem : Decoder Item
-decodeItem =
-    Decode.map2 Item
-        (Decode.field "title" Decode.string)
-        (Decode.maybe (Decode.field "story" decodeStory))
-
-
-main : Html msg
+main : Program () Model Msg
 main =
-    """
-{"title":"Create New Page Test","story":[],"journal":[{"type":"create","item":{"title":"Create New Page Test","story":[]},"date":1685640550036}]}
-"""
-        |> Decode.decodeString decodePage
-        |> Debug.toString
-        |> Html.text
+    Browser.element { init = init, update = update, view = view, subscriptions = subscriptions }
+
+
+type alias Model =
+    { jsonText : String
+    }
+
+
+type Msg
+    = NoOp
+
+
+init : () -> ( Model, Cmd Msg )
+init _ =
+    ( { jsonText = "Put your slug.json content here" }, Cmd.none )
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        NoOp ->
+            ( model, Cmd.none )
+
+
+subscriptions : Model -> Sub Msg
+subscriptions _ =
+    Sub.none
+
+
+view : Model -> Html Msg
+view model =
+    let
+        bracketStructure =
+            case Json.decodeString getBracketStructure model.jsonText of
+                Ok brackets ->
+                    brackets
+
+                Err error ->
+                    "Invalid JSON: " ++ Json.errorToString error
+    in
+    div []
+        [ pre [] [ text bracketStructure ]
+        ]
+
+
+getBracketStructure : Json.Decoder String
+getBracketStructure =
+    Json.succeed "Bracket structure placeholder"
