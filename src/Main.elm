b6cf5e3baac8ec259a71a39dab4334d10b779443
RalfBarkow <ralf.barkow@me.com>
2023-09-11 13:36:24 +0200

    Parse wiki-style links and render them as anchors
    Ref: https://wiki.ralfbarkow.ch/view/vite-elm-dojo/view/parse-wiki-style-links/view/parse-wiki-style-links_rev17

--- src/Main.elm
+++ src/Main.elm
@@ -1,118 +1,53 @@
-module Main exposing (Model, Msg, ParsedJson, init, main, update, view)
+module Main exposing (main)
 
-import Browser
-import Html exposing (Html, button, div, h3, pre, text, textarea)
-import Html.Attributes exposing (cols, placeholder, rows)
-import Html.Events exposing (onClick, onInput)
-import Json.Decode as Decode
-import Json.Encode as Encode
-import String exposing (trim)
-import Wiki
+import Html exposing (Html, a, text)
+import Html.Attributes exposing (href)
+import String exposing (contains, split)
 
 
-type alias Model =
-    { input : String
-    , parsedJson : ParsedJson
-    , output : String
+type alias WikiLink =
+    { label : String
+    , target : String
     }
 
 
-type ParsedJson
-    = NotParsed
-    | Parsed Wiki.Page
-
-
-init : () -> ( Model, Cmd Msg )
-init _ =
+parseWikiLink : String -> Maybe WikiLink
+parseWikiLink input =
     let
-        rawData : String.String
-        rawData =
-            """
-{
-  "title": "2023-06-12",
-  "story": [],
-  "journal": [
-    {
-      "type": "create",
-      "item": {
-        "title": "2023-06-12",
-        "story": []
-      },
-      "date": 1686550113460
-    }
-  ]
-}
-            """
+        parts =
+            split "]]" input
     in
-    ( { input = rawData, parsedJson = NotParsed, output = "" }, Cmd.none )
+    case parts of
+        [ label, target ] ->
+            if contains "[[" label then
+                Just { label = String.dropLeft 2 label, target = target }
+
+            else
+                Nothing
+
+        _ ->
+            Nothing
 
 
-type Msg
-    = UpdateInput String
-    | ParseJson
+renderWikiLink : WikiLink -> Html msg
+renderWikiLink link =
+    a [ Html.Attributes.href link.target ] [ Html.text link.label ]
 
 
-update : Msg -> Model -> ( Model, Cmd Msg )
-update msg model =
-    case msg of
-        UpdateInput value ->
-            ( { model | input = value }, Cmd.none )
-
-        ParseJson ->
-            let
-                json : String.String
-                json =
-                    trim model.input
-            in
-            case Decode.decodeString Wiki.pageDecoder json of
-                Ok value ->
-                    ( { model
-                        | parsedJson = Parsed value
-                        , output = Wiki.pageEncoder value |> Encode.encode 0
-                      }
-                    , Cmd.none
-                    )
-
-                Err _ ->
-                    ( { model | parsedJson = NotParsed, output = "" }, Cmd.none )
-
-
-view : Model -> Html Msg
-view model =
-    div []
-        [ div []
-            -- UpdateInput
-            [ textarea [ placeholder "Enter JSON here", rows 10, cols 80, onInput UpdateInput ] [ text model.input ]
-            ]
-        , div []
-            -- ParseJson button
-            [ button [ onClick ParseJson ] [ text "Parse JSON" ]
-            ]
-        , div []
-            -- ParseJson feedback
-            [ case model.parsedJson of
-                NotParsed ->
-                    text "JSON not parsed. Please enter your data and click 'Parse JSON'."
-
-                Parsed page ->
-                    div []
-                        [ h3 [] [ text "Parsed JSON" ]
-                        , pre [] [ text (Debug.toString page) ]
-                        ]
-            ]
-        , div []
-            -- Output
-            [ h3 [] [ text "Output" ]
-            , pre [] [ text model.output ]
-            ]
-        ]
-
-
-main : Program () Model Msg
+main : Html msg
 main =
-    Browser.element
-        { init = init
-        , view = view
-        , update = update
-        , subscriptions = \_ -> Sub.none
-        }
+    let
+        wikiLinkText : String
+        wikiLinkText =
+            "[[OpenAI]]"
+
+        maybeWikiLink : Maybe WikiLink
+        maybeWikiLink =
+            parseWikiLink wikiLinkText
+    in
+    case maybeWikiLink of
+        Just link ->
+            renderWikiLink link
+
+        Nothing ->
+            text "Invalid Wiki Link"
