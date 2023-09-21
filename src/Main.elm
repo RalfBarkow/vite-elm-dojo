63410fc5b98b95fe1f45ee9992038633d24618cc
RalfBarkow <ralf.barkow@me.com>
2023-09-21 14:36:56 +0200

    refactor: reformat code

--- src/Main.elm
+++ src/Main.elm
@@ -1,21 +1,29 @@
 module Main exposing (..)
 
-import Browser  
+import Browser
 import Html exposing (..)
-import Html.Events exposing (onInput)
 import Html.Attributes exposing (placeholder, value)
+import Html.Events exposing (onInput)
+
+
 
 -- MAIN
+
+
 main : Program () Model Msg
 main =
-  Browser.sandbox { init = init, update = update, view = view }
+    Browser.sandbox { init = init, update = update, view = view }
+
+
 
 -- MODEL
+
 
 type alias Model =
     { input : String
     , output : String
     }
+
 
 init : Model
 init =
@@ -23,11 +31,13 @@ init =
     , output = ""
     }
 
+
+
 -- UPDATE
+
 
 type Msg
     = ParseInput String
-
 
 
 update : Msg -> Model -> Model
@@ -35,13 +45,22 @@ update msg model =
     case msg of
         ParseInput input ->
             let
-                tokens = tokenize input
-                expression = parseExpression tokens
-                result = showExpression expression
+                tokens =
+                    tokenize input
+
+                expression =
+                    parseExpression tokens
+
+                result =
+                    showExpression expression
             in
             { model | input = input, output = result }
 
+
+
 -- VIEW
+
+
 view : Model -> Html Msg
 view model =
     div []
@@ -49,7 +68,6 @@ view model =
         , textarea [ onInput ParseInput, placeholder "Enter expression", value model.input ] []
         , div [] [ text ("Result: " ++ model.output), div [ Html.Attributes.id "result" ] [] ]
         ]
-
 
 
 type Expression
@@ -69,71 +87,113 @@ type Token
 
 tokenize : String -> List Token
 tokenize input =
-    List.map (\token -> 
-        case String.toInt token of
-            Just value -> ValueToken value
-            Nothing -> 
-                if String.contains "+" token then
-                    OperatorToken "+"
-                else if String.contains "-" token then
-                    OperatorToken "-"
-                else if String.contains "*" token then
-                    OperatorToken "*"
-                else if String.contains "/" token then
-                    OperatorToken "/"
-                else
-                    VariableToken token
-    ) (String.split " " input)
+    List.map
+        (\token ->
+            case String.toInt token of
+                Just value ->
+                    ValueToken value
+
+                Nothing ->
+                    if String.contains "+" token then
+                        OperatorToken "+"
+
+                    else if String.contains "-" token then
+                        OperatorToken "-"
+
+                    else if String.contains "*" token then
+                        OperatorToken "*"
+
+                    else if String.contains "/" token then
+                        OperatorToken "/"
+
+                    else
+                        VariableToken token
+        )
+        (String.split " " input)
+
 
 parseExpression : List Token -> Expression
 parseExpression tokens =
     let
-        (expression, _) = parseExpressionHelper tokens
+        ( expression, _ ) =
+            parseExpressionHelper tokens
     in
     expression
 
-parseExpressionHelper : List Token -> (Expression, List Token)
+
+parseExpressionHelper : List Token -> ( Expression, List Token )
 parseExpressionHelper tokens =
     case tokens of
-        [] -> (Value 0, [])
-        ValueToken value :: rest -> (Value value, rest)
-        VariableToken variable :: rest -> (Variable variable, rest)
-        OperatorToken "+" :: rest ->
-            let
-                (left, rest1) = parseExpressionHelper rest
-                (right, rest2) = parseExpressionHelper rest1
-            in
-            (Add left right, rest2)
-        OperatorToken "-" :: rest ->
-            let
-                (left, rest1) = parseExpressionHelper rest
-                (right, rest2) = parseExpressionHelper rest1
-            in
-            (Subtract left right, rest2)
-        OperatorToken "*" :: rest ->
-            let
-                (left, rest1) = parseExpressionHelper rest
-                (right, rest2) = parseExpressionHelper rest1
-            in
-            (Multiply left right, rest2)
-        OperatorToken "/" :: rest ->
-            let
-                (left, rest1) = parseExpressionHelper rest
-                (right, rest2) = parseExpressionHelper rest1
-            in
-            (Divide left right, rest2)
-        _ -> (Value 0, tokens)
+        [] ->
+            ( Value 0, [] )
 
+        (ValueToken value) :: rest ->
+            ( Value value, rest )
 
+        (VariableToken variable) :: rest ->
+            ( Variable variable, rest )
+
+        (OperatorToken "+") :: rest ->
+            let
+                ( left, rest1 ) =
+                    parseExpressionHelper rest
+
+                ( right, rest2 ) =
+                    parseExpressionHelper rest1
+            in
+            ( Add left right, rest2 )
+
+        (OperatorToken "-") :: rest ->
+            let
+                ( left, rest1 ) =
+                    parseExpressionHelper rest
+
+                ( right, rest2 ) =
+                    parseExpressionHelper rest1
+            in
+            ( Subtract left right, rest2 )
+
+        (OperatorToken "*") :: rest ->
+            let
+                ( left, rest1 ) =
+                    parseExpressionHelper rest
+
+                ( right, rest2 ) =
+                    parseExpressionHelper rest1
+            in
+            ( Multiply left right, rest2 )
+
+        (OperatorToken "/") :: rest ->
+            let
+                ( left, rest1 ) =
+                    parseExpressionHelper rest
+
+                ( right, rest2 ) =
+                    parseExpressionHelper rest1
+            in
+            ( Divide left right, rest2 )
+
+        _ ->
+            ( Value 0, tokens )
 
 
 showExpression : Expression -> String
 showExpression expression =
     case expression of
-        Value value -> String.fromInt value
-        Variable variable -> variable
-        Add left right -> "(" ++ showExpression left ++ " + " ++ showExpression right ++ ")"
-        Subtract left right -> "(" ++ showExpression left ++ " - " ++ showExpression right ++ ")"
-        Multiply left right -> "(" ++ showExpression left ++ " * " ++ showExpression right ++ ")"
-        Divide left right -> "(" ++ showExpression left ++ " / " ++ showExpression right ++ ")"
+        Value value ->
+            String.fromInt value
 
+        Variable variable ->
+            variable
+
+        Add left right ->
+            "(" ++ showExpression left ++ " + " ++ showExpression right ++ ")"
+
+        Subtract left right ->
+            "(" ++ showExpression left ++ " - " ++ showExpression right ++ ")"
+
+        Multiply left right ->
+            "(" ++ showExpression left ++ " * " ++ showExpression right ++ ")"
+
+        Divide left right ->
+            "(" ++ showExpression left ++ " / " ++ showExpression right ++ ")"
