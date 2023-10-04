deefaf517a5b3fb82a662a66975c19d4b61483a5
RalfBarkow <ralf.barkow@me.com>
2023-10-04 13:49:09 +0200

    fix: grammarString (GREEN phase)

--- src/Main.elm
+++ src/Main.elm
@@ -15,23 +15,8 @@ import Peg
 grammarString : String
 grammarString =
     """
-Grammar <- ws* Value ws*
-
-Value    <- ws* (Object / Array / Number / String 
-          / True / False / Null) ws*
-
-Object  <- '{' ws* Member (ws* ',' ws* Member)* ws* '}'
-Array   <- '[' ws* Value (ws* ',' ws* Value)* ws* ']'
-Number  <- MINUS? Integer (DOT DIGIT+)? ('e' / 'E') sign DIGIT+ 
-String  <- DQ (Escaped / [x20-x21] / [x23-x5B] / [x5D-x10FFFF])* DQ
-True    <- 'true'
-False   <- 'false'
-Null    <- 'null'
-
-Member  <- String ':' Value
-Integer  <- '0' / [1-9] DIGIT*
-Escaped  <- BKSLASH ('b' / 'f' / 'n' / 'r' / 't' / 'u' hex{4}
-          / DQ / BKSLASH / SLASH)
+    start <- <char+> {action}
+    char <- [a-z]        
     """
 
 
@@ -48,7 +33,7 @@ result =
     in
     grammarString
         |> fromString
-        |> Result.andThen (\grammar -> parse grammar "" actions predicate "{}")
+        |> Result.andThen (\grammar -> parse grammar "" actions predicate "abc")
 
 
 {-| Check if the parse succeeded
